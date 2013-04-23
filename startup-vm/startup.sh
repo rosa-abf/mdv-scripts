ba7eb67f96159640b3b52720b383bcec0fbef66d
Vokhmin Alexey V <avokhmin@gmail.com>
2013-04-23 13:21:16 +0400

    #5: rollback changes

--- startup-vm/startup.sh
+++ startup-vm/startup.sh
@@ -4,7 +4,5 @@ sudo urpmi.update -a
 for p in urpmi mock-urpm genhdlist2 tree nfs-utils ; do
   sudo urpmi --auto $p
 done
-sudo /bin/bash -c 'echo "185.4.234.68 file-store.rosalinux.ru" >> /etc/hosts'
-sudo /bin/bash -c 'echo "195.19.76.241 abf.rosalinux.ru" >> /etc/hosts'
 
 exit 0
