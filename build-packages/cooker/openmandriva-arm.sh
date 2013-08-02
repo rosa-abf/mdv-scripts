912826f5b0f31d0e0cd510302f9877b83f7bad66
Alexander Khryukin <alexander@mezon.ru>
2013-08-02 20:46:21 +0400

    disable noscripts option and sleep for copying qemu :D

--- build-packages/cooker/openmandriva-arm.sh
+++ build-packages/cooker/openmandriva-arm.sh
@@ -93,6 +93,11 @@ sudo sh -c "echo '$platform_arch-mandriva-linux-gnueabi' > /etc/rpm/platform"
 sudo sh -c "echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-wrapper:' > /proc/sys/fs/binfmt_misc/register"
 
 
+# copy qemu binaries
+# it is big ugly hack
+# but i do now know another proper way
+# experiments with setup.spec was not successful
+sleep 25 && sudo cp $rpm_build_script_path/cooker/qemu*  $tmpfs_path/openmandriva-2013.0-$platform_arch/root/usr/bin/ &
 # Build src.rpm
 echo '--> Build src.rpm'
 mock-urpm --buildsrpm --spec $tmpfs_path/SPECS/$spec_name --sources $tmpfs_path/SOURCES/ --resultdir $src_rpm_path --configdir $config_dir -v --no-cleanup-after
