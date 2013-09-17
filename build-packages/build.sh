e65e0029542ac3ea03edec8ee1106ceea8fe42c4
Alexey Vokhmin <avokhmin@gmail.com>
2013-09-17 14:27:32 +0400

    Hot fix

--- build-packages/build.sh
+++ build-packages/build.sh
@@ -60,13 +60,6 @@ UNAME="$UNAME" EMAIL="$EMAIL" PLATFORM_NAME="$PLATFORM_NAME" PLATFORM_ARCH="$ARC
   exit $rc
 fi
 
-if [ "$platform_name" == 'rosa-dx-chrome-1.0' ] ; then
-  # TODO: remove when "main" repository will be rebuilt
-  echo "rosa2012lts_main_release http://abf-downloads.rosalinux.ru/rosa2012lts/repository/$platform_arch/main/release" >> $media_list
-  echo "rosa2012lts_contrib_release http://abf-downloads.rosalinux.ru/rosa2012lts/repository/$platform_arch/contrib/release" >> $media_list
-  echo "rosa2012lts_contrib_updates http://abf-downloads.rosalinux.ru/rosa2012lts/repository/$platform_arch/contrib/updates" >> $media_list
-fi
-
 # create SPECS folder and move *.spec
 mkdir $tmpfs_path/SPECS
 mv $project_path/*.spec $tmpfs_path/SPECS/
@@ -110,6 +103,13 @@ fi
 default_cfg=$rpm_build_script_path/configs/default.cfg
 cp $rpm_build_script_path/configs/$config_name $default_cfg
 media_list=/home/vagrant/container/media.list
+
+if [ "$platform_name" == 'rosa-dx-chrome-1.0' ] ; then
+  # TODO: remove when "main" repository will be rebuilt
+  echo "rosa2012lts_main_release http://abf-downloads.rosalinux.ru/rosa2012lts/repository/$platform_arch/main/release" >> $media_list
+  echo "rosa2012lts_contrib_release http://abf-downloads.rosalinux.ru/rosa2012lts/repository/$platform_arch/contrib/release" >> $media_list
+  echo "rosa2012lts_contrib_updates http://abf-downloads.rosalinux.ru/rosa2012lts/repository/$platform_arch/contrib/updates" >> $media_list
+fi
 
 echo "config_opts['macros']['%packager'] = '$uname <$email>'" >> $default_cfg
 
