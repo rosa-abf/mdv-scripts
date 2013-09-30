d4dabf4a2d18b92d24581ef30988b0af221c7845
Alexey Vokhmin <avokhmin@gmail.com>
2013-09-30 21:47:45 +0400

    Added debug info

--- build-packages/build.sh
+++ build-packages/build.sh
@@ -68,6 +68,7 @@ if [ "$uname" == 'avokhmin' ] ; then
   # build changelog (limited to ~10 for reasonable changelog size)
   sed -i '/%changelog/,$d' $spec_name
   echo '%changelog' >> $spec_name
+  echo "python $rpm_build_script_path/build-changelog.py -b 5 -e $commit_hash -n $spec_name >> $spec_name"
   python $rpm_build_script_path/build-changelog.py -b 5 -e $commit_hash -n $spec_name >> $spec_name
 fi
 
