df4d4daff2681c7d171e451a8e73542f88d19491
Alexey Vokhmin <avokhmin@gmail.com>
2013-10-01 12:51:49 +0400

    Enable changelog

--- build-packages/build.sh
+++ build-packages/build.sh
@@ -63,14 +63,16 @@ else
   fi
 fi
 
-# TODO: remove later
-if [ "$uname" == 'robxu9' ] ; then
-  # build changelog (limited to ~10 for reasonable changelog size)
-  sed -i '/%changelog/,$d' $spec_name
-  echo '%changelog' >> $spec_name
-  echo "python $rpm_build_script_path/build-changelog.py -b 5 -e $commit_hash -n $spec_name >> $spec_name"
-  python $rpm_build_script_path/build-changelog.py -b 5 -e $commit_hash -n $spec_name >> $spec_name
-fi
+
+# build changelog (limited to ~10 for reasonable changelog size)
+sed -i '/%changelog/,$d' $spec_name
+echo '%changelog' >> $spec_name
+changelog_log=$results_path/changelog.log
+echo "python $rpm_build_script_path/build-changelog.py -b 5 -e $commit_hash -n $spec_name >> $changelog_log"
+python $rpm_build_script_path/build-changelog.py -b 5 -e $commit_hash -n $spec_name >> $changelog_log
+echo "cat $changelog_log >> $spec_name"
+cat $changelog_log >> $spec_name
+
 
 # Remove .git folder
 rm -rf $project_path/.git
