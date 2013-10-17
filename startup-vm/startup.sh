48ef9c68464796b707d6e6b48f7ee9f011cf0ed0
Alex Burmashev <alex.burmashev@rosalab.ru>
2013-10-17 12:38:46 +0400

    update curl also

--- startup-vm/startup.sh
+++ startup-vm/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo urpmi.update -a
-for p in urpmi perl-URPM mock-urpm genhdlist2 tree ; do
+for p in curl urpmi perl-URPM mock-urpm genhdlist2 tree ; do
   sudo urpmi --auto $p
 done
 
