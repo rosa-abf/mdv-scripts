ec2d3628b809439d320d75485da71b2846af3da8
Vokhmin Alexey V <avokhmin@gmail.com>
2013-10-19 00:57:48 +0400

    #25: added  --no-suggests --no-verify-rpm options into urpmi

--- startup-vm/startup.sh
+++ startup-vm/startup.sh
@@ -2,7 +2,7 @@
 sudo /bin/bash -c 'echo "195.19.76.240 abf-downloads.rosalinux.ru" >> /etc/hosts'
 sudo urpmi.update -a
 for p in curl urpmi perl-URPM mock-urpm genhdlist2 tree git rpm ruby ; do
-  sudo urpmi --auto $p
+  sudo urpmi --no-suggests --no-verify-rpm --auto $p
 done
 
 # sudo usermod -a -G vboxsf vagrant
