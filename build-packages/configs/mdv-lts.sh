9be71c7f142d9572291a6ba8fed7d1e591eed81a
Alexey Vokhmin <avokhmin@gmail.com>
2013-11-06 15:54:32 +0400

    added --fastunsafe option into mdv-lts.sh

--- build-packages/configs/mdv-lts.sh
+++ build-packages/configs/mdv-lts.sh
@@ -25,7 +25,7 @@ EOF
 fi
 
 cat <<EOF>> $default_cfg
-config_opts['urpmi_options'] = '--no-suggests --no-verify-rpm --ignoresize $extra_cfg_options'
+config_opts['urpmi_options'] = '--no-suggests --no-verify-rpm --fastunsafe --ignoresize $extra_cfg_options'
 config_opts['urpm_options'] = ''
 
 # If it's True - current urpmi configs will be copied to the chroot.
