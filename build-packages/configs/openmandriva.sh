8db8c5ed5a0727919cccbbd7dd7a7a26dc91541e
Alexey Vokhmin <avokhmin@gmail.com>
2013-11-06 15:55:45 +0400

    added --fastunsafe option into openmandriva.sh

--- build-packages/configs/openmandriva.sh
+++ build-packages/configs/openmandriva.sh
@@ -14,7 +14,7 @@ config_opts['target_arch'] = '$platform_arch --without check'
 config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64', 'armv7l', 'armv7hl')
 
 config_opts['chroot_setup'] = 'basesystem-minimal locales locales-en locales-de locales-uk locales-es locales-ru distro-release-OpenMandriva gnupg rpm-build urpmi meta-task'
-config_opts['urpmi_options'] = '--no-suggests --no-verify-rpm --ignoresize --ignorearch --excludedocs'
+config_opts['urpmi_options'] = '--no-suggests --no-verify-rpm --fastunsafe --ignoresize --ignorearch --excludedocs'
 config_opts['urpm_options'] = ''
 
 # If it's True - current urpmi configs will be copied to the chroot.
