a3188647f00bada0778797a2a31dcd7ae6ca316d
Alexander Khryukin <alexander@mezon.ru>
2014-01-23 14:54:14 +0000

    add ignorearch option to bypass buggy urpmi

--- startup-vm/startup.sh
+++ startup-vm/startup.sh
@@ -2,7 +2,7 @@
 sudo /bin/bash -c 'echo "195.19.76.240 abf-downloads.rosalinux.ru" >> /etc/hosts'
 sudo urpmi.update -a
 PACKAGES=(wget curl urpmi perl-URPM mock-urpm genhdlist2 tree git rpm ruby)
-sudo urpmi ${PACKAGES[*]} --auto --no-suggests --no-verify-rpm
+sudo urpmi ${PACKAGES[*]} --auto --no-suggests --no-verify-rpm --ignorearch
 
 # sudo usermod -a -G vboxsf vagrant
 sudo usermod -a -G  mock-urpm vagrant
