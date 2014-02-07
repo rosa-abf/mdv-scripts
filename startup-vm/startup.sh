8c0140f76dcba0ca27eaa024258c17b4449982a4
Vokhmin Alexey V <avokhmin@gmail.com>
2014-02-07 15:06:13 +0400

    Configure proxy

--- startup-vm/startup.sh
+++ startup-vm/startup.sh
@@ -8,4 +8,13 @@ sudo urpmi ${PACKAGES[*]} --auto --no-suggests --no-verify-rpm --ignorearch
 sudo usermod -a -G  mock-urpm vagrant
 sudo cp -f  /usr/share/zoneinfo/UTC /etc/localtime
 
+# ABF_DOWNLOADS_PROXY, see: /etc/profile
+if [ "$ABF_DOWNLOADS_PROXY" != '' ] ; then
+  sudo /bin/bash -c "echo 'export http_proxy=$ABF_DOWNLOADS_PROXY' >> /etc/profile"
+
+  sudo /bin/bash -c "echo 'http_proxy=$ABF_DOWNLOADS_PROXY'   >> /etc/urpmi/proxy.cfg"
+  sudo /bin/bash -c "echo 'ftp_proxy=$ABF_DOWNLOADS_PROXY'    >> /etc/urpmi/proxy.cfg"
+  sudo /bin/bash -c "echo 'https_proxy=$ABF_DOWNLOADS_PROXY'  >> /etc/urpmi/proxy.cfg"
+fi
+
 exit 0
