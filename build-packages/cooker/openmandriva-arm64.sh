e75aa76e97587dbd992b4366ffcf505abd226994
Alexander Khryukin <alexander@mezon.ru>
2014-03-13 12:16:22 +0000

    clean env

--- build-packages/cooker/openmandriva-arm64.sh
+++ build-packages/cooker/openmandriva-arm64.sh
@@ -89,7 +89,7 @@ sudo sh -c "echo '$platform_arch-mandriva-linux-gnueabi' > /etc/rpm/platform"
 # if path to qemu binary changed
 # uncomment this line and start a massbuild
 # to rewrite /proc env on build nodes
-#sudo sh -c "echo '-1' > /proc/sys/fs/binfmt_misc/$platform_arch"
+sudo sh -c "echo '-1' > /proc/sys/fs/binfmt_misc/$platform_arch"
 # echo new wrapper
 sudo sh -c "echo ':aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7:\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/usr/bin/qemu-aarch64-binfmt:P' > /proc/sys/fs/binfmt_misc/register"
 
