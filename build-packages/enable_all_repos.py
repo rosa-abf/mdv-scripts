8ddc109e08d64b9a7824b58615da4da8075582bf
Denis Silakov <denis.silakov@rosalab.ru>
2014-06-25 12:50:43 +0400

    Use --all-media option, add SRPMS repositories

--- build-packages/enable_all_repos.py
+++ build-packages/enable_all_repos.py
@@ -18,13 +18,26 @@ mirrorlist  = sys.argv[2]
 
 # Add all distribution media
 print(" ... updating distribution list from " + mirrorlist)
-os.system("sudo chroot " + chroot_path + " urpmi.addmedia --wget --wget-options --auth-no-challenge --debug --distrib --mirrorlist " + mirrorlist)
+os.system("sudo chroot " + chroot_path + " urpmi.addmedia --wget --wget-options --auth-no-challenge --debug --distrib --all-media --mirrorlist " + mirrorlist)
 
+# No need in this - '--all-media' option should enable all repositories
 # Enable and update ignored media
-active_media = os.popen("sudo chroot " + chroot_path + " urpmq --wget --wget-options --auth-no-challenge --debug --list-media active").readlines()
-p = os.popen("sudo chroot " + chroot_path + " urpmq --wget --wget-options --auth-no-challenge --debug --list-media")
+#active_media = os.popen("sudo chroot " + chroot_path + " urpmq --wget --wget-options --auth-no-challenge --debug --list-media active").readlines()
+#p = os.popen("sudo chroot " + chroot_path + " urpmq --wget --wget-options --auth-no-challenge --debug --list-media")
+#for rep in p.readlines():
+#    if rep not in active_media:
+#	rep = rep.rstrip()
+#        os.system("sudo chroot " + chroot_path + " urpmi.update --wget --wget-options --auth-no-challenge --debug --no-ignore '" + rep + "'")
+#        os.system("sudo chroot " + chroot_path + " urpmi.update --wget --wget-options --auth-no-challenge --debug '" + rep + "'")
+
+# Add SRPMS media
+p = os.popen("sudo chroot " + chroot_path + " urpmq --wget --wget-options --auth-no-challenge --list-url")
 for rep in p.readlines():
-    if rep not in active_media:
-	rep = rep.rstrip()
-        os.system("sudo chroot " + chroot_path + " urpmi.update --wget --wget-options --auth-no-challenge --debug --no-ignore '" + rep + "'")
-        os.system("sudo chroot " + chroot_path + " urpmi.update --wget --wget-options --auth-no-challenge --debug '" + rep + "'")
+    url = rep.split(" ")[-1]
+    url = url.replace("i586/media","SRPMS")
+    url = url.replace("x86_64/media","SRPMS")
+    rep = rep.replace(url,"")
+    os.system("sudo chroot " + chroot_path + " urpmi.addmedia --wget --wget-options --auth-no-challenge --debug '" + rep + " srpms' "  + url)
+
+print("The following repositories will be used to look for dependent packages:")
+p = os.popen("sudo chroot " + chroot_path + " urpmq --wget --wget-options --auth-no-challenge --list-url")
