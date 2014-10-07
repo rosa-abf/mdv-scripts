44c2ef6d5fba4aeb5f39e89d65a916a078912e08
Denis Silakov <denis.silakov@rosalab.ru>
2014-10-07 23:19:37 +0400

    Retry to update urpmi & co in case of failure

--- startup-vm/startup.sh
+++ startup-vm/startup.sh
@@ -3,9 +3,34 @@
 sudo /bin/bash -c 'echo noop > /sys/block/vda/queue/scheduler'
 
 sudo /bin/bash -c 'echo "195.19.76.240 abf-downloads.rosalinux.ru" >> /etc/hosts'
+
 sudo urpmi.update -a
 PACKAGES=(wget curl urpmi perl-URPM mock-urpm genhdlist2 tree git rpm ruby python-rpm5utils python-rpm urpm-tools)
-sudo urpmi ${PACKAGES[*]} --downloader wget --wget-options --auth-no-challenge --auto --no-suggests --no-verify-rpm --ignorearch
+
+# We will rerun update of crucial packages in case when repository is modified in the middle,
+# but for safety let's limit number of retest attempts
+# (since in case when repository metadata is really broken we can loop here forever)
+MAX_RETRIES=5
+WAIT_TIME=300
+RETRY_GREP_STR="You may need to update your urpmi database\|problem reading synthesis file of medium\|retrieving failed: "
+
+try_rerun=true
+retry=0
+while $try_rerun
+do
+  sudo urpmi ${PACKAGES[*]} --downloader wget --wget-options --auth-no-challenge --auto --no-suggests --no-verify-rpm --ignorearch > /tmp/update.log 2>&1
+  test_code=$?
+  try_rerun=false
+  if [[ $test_code != 0 && $retry < $MAX_RETRIES ]] ; then
+    if grep -q "$RETRY_GREP_STR" /tmp/update.log; then
+      echo '--> Repository was changed in the middle, will relaunch update of crucial packages'
+      sleep $WAIT_TIME
+      sudo urpmi.update -a
+      try_rerun=true
+      (( retry=$retry+1 ))
+    fi
+  fi
+done
 
 # sudo usermod -a -G vboxsf vagrant
 sudo usermod -a -G  mock-urpm vagrant
